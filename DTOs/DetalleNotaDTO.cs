176a5cdc8943ab36e9cf8948a87f3b339b2f82bc
Gildardo Alvarado <leonelgildardogl@hotmail.com>
2021-07-29 21:33:38 -0600

    create DTOs

---
+++ DTOs/DetalleNotaDTO.cs
@@ -0,0 +1,10 @@
+namespace ApiControlDeColegio.DTOs
+{
+    public class DetalleNotaDTO
+    {
+        public string DetalleNotaId { get; set; }
+        public string DetalleActividadId { get; set; }
+        public string Carne { get; set; }
+        public int ValorNota { get; set; }
+    }
+}
